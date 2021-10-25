82b7899524994037a4243446eb20b7ac547d3b32
RonaldoXVIII <skytchname@gmail.com>
2021-10-24 22:16:51 -0500

    PorProceso:Done

--- Sistemas_Costos/src/costo_por_proceso/Costo.java
+++ Sistemas_Costos/src/costo_por_proceso/Costo.java
@@ -17,6 +17,9 @@ public class Costo {
         this.costoConversion = costoConversion;
     }
     
-    
+    public  double costoTotal()
+    {
+        return costoMaterial+costoConversion;
+    }
     
 }
--- Sistemas_Costos/src/costo_por_proceso/Departamento.java
+++ Sistemas_Costos/src/costo_por_proceso/Departamento.java
@@ -17,6 +17,10 @@ public class Departamento {
     Costo costoInicial,costoFinal;
     Unidad uep_inicial;
     Unidad uep_final;
+    double totalCosteado,ueMateriales,ueCConversion;
+    double costoContabilizado,costoContabilizar;
+    
+    
 
     public Departamento(String nombre, Unidad unidadesTerminadas, Unidad unidadesIniciadas, Costo costoInicial, Costo costoFinal, Unidad uep_inicial, Unidad uep_final) {
         this.nombre = nombre;
@@ -26,9 +30,157 @@ public class Departamento {
         this.costoFinal = costoFinal;
         this.uep_inicial = uep_inicial;
         this.uep_final = uep_final;
+        
+        
+        
+        this.totalCosteado = uep_final.cantidad + unidadesTerminadas.cantidad;
+        
     }
     
     
+    
+    public double margenDeError()
+    {
+        return this.costoContabilizado-this.costoContabilizar;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void costoPorContabilizado(boolean esPeps) {
+        if(esPeps) {
+            this.costoContabilizado = costoInicial.costoTotal() +
+                    (uep_inicial.cantidad*(1-uep_inicial.tasa_cm))*costoE_porUnidadMaterial(esPeps)+
+                    (uep_inicial.cantidad*(1-uep_inicial.tasa_cc))*costoE_porUnidadCC(esPeps)+
+                    unidades_cyt()*costoE_Total(esPeps)+
+                    uep_final.cantidad*uep_final.tasa_cm*costoE_porUnidadMaterial(esPeps) + 
+                   uep_final.cantidad*uep_final.tasa_cc*costoE_porUnidadCC(esPeps) ;
+            
+            
+        }
+        else {
+        
+           this.costoContabilizado =  unidadesTerminadas.cantidad*costoE_Total(esPeps) +
+                   uep_final.cantidad*uep_final.tasa_cm*costoE_porUnidadMaterial(esPeps) + 
+                   uep_final.cantidad*uep_final.tasa_cc*costoE_porUnidadCC(esPeps) ;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public double costosContabilizarMateriales()
+    {
+        return costoInicial.costoMaterial + costoFinal.costoMaterial;
+        
+    }
+    
+    public double costosContabilizarCC()
+    {
+        return costoInicial.costoConversion + costoFinal.costoConversion;
+    }
+    
+   public double costoPorContabilizar()
+   {
+       this.costoContabilizar = costosContabilizarCC()+costosContabilizarMateriales();
+       return this.costoContabilizar;
+   }
+   
+   
+   
+   public double costoE_porUnidadMaterial(boolean esPeps)
+   {
+       if(esPeps)
+       {
+           return costoFinal.costoMaterial/ueMateriales;
+       } else {
+           return costosContabilizarMateriales()/ueMateriales;
+       }
+       
+   }
+   public double costoE_porUnidadCC(boolean esPeps)
+   {
+        if(esPeps)
+       {
+           return costoFinal.costoConversion/ueCConversion;
+       } else {
+           return costosContabilizarCC()/ueCConversion;
+       }
+   }
+   
+   
+   public double costoE_Total(boolean esPeps)
+   {
+       return costoE_porUnidadCC(esPeps) +costoE_porUnidadMaterial(esPeps);
+   }
+   
+ 
+   
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void unidades_equivalentes(boolean esPeps)
+    {
+        if(esPeps)
+        {
+            this.ueMateriales = unidades_cyt() + uep_inicial.cantidad*(1-uep_inicial.tasa_cm)  + uep_final.cantidad*uep_final.tasa_cm;
+            this.ueCConversion = unidades_cyt() + uep_inicial.cantidad*(1-uep_inicial.tasa_cc)  + uep_final.cantidad*uep_final.tasa_cc;
+           
+        } else {
+            this.ueMateriales = unidadesTerminadas.tasa_cm*unidadesTerminadas.cantidad + uep_final.cantidad*uep_final.tasa_cm;
+            this.ueCConversion = unidadesTerminadas.tasa_cc*unidadesTerminadas.cantidad + uep_final.cantidad*uep_final.tasa_cc;
+        }
+    }
+    
+    
+    
+    public double unidades_comenzadas()
+    {
+        return totalCosteado - uep_inicial.cantidad; 
+    }
+    
+    
+    public double unidades_cyt()
+    {
+        return totalCosteado-(uep_final.cantidad+uep_inicial.cantidad);
+    }
     
             
             
