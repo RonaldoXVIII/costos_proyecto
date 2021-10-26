b1a3141b1b9e882e40b2af4484f4d04f4d252e90
RonaldoXVIII <skytchname@gmail.com>
2021-10-25 22:58:11 -0500

    fixing

--- Sistemas_Costos/src/costeo_por_abc/Proceso.java
+++ Sistemas_Costos/src/costeo_por_abc/Proceso.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Costeo_ABC;
+package costeo_por_abc;
 
 import java.util.ArrayList;
 import java.util.function.BiPredicate;
